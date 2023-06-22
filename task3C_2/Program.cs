1aab7efcb7bc62983e4c8d38a28bfd97c9944869
Tatyana <vta278@mail.ru>
2023-06-22 23:51:53 +0300

    добавлена задача3 таблица кубов числа

---
+++ task3C_2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+
+int ReadInt(string message)
+{
+    System.Console.WriteLine(message);
+    return int.Parse(Console.ReadLine());
+}
+
+int N = ReadInt("Введите число:");
+for (int i = 1; i < N+1; i++)
+{
+   System.Console.WriteLine( i*i*i);
+}
